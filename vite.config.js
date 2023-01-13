d3638b56b8f68332c3471ca8d45e595263b811a8
liangchen25 <liangchen25@iflytek.com>
2023-01-13 14:31:17 +0800

    配置cdn

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,63 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
 
 // 定义 webpack 不打包资源列表
-const externals = {
-  vue: 'Vue',
-  vuex: 'Vuex',
-  axios: 'axios',
-  'vue-router': 'VueRouter',
-}
+// const externals = {
+//   vue: 'Vue',
+//   vuex: 'Vuex',
+//   axios: 'axios',
+//   'vue-router': 'VueRouter',
+// }
+
+console.log(importToCDN)
 
 // https://vitejs.dev/config/
 export default defineConfig(async (command, mode) => {
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      importToCDN({
+        modules: [
+          {
+            name: 'vue',
+            var: 'Vue',
+            path: 'https://unpkg.com/vue@3.2.45/dist/vue.global.js'
+          },
+          {
+            name: 'vue-router',
+            var: 'VueRouter',
+            path: 'https://unpkg.com/vue-router@4.1.6/dist/vue-router.global.js'
+          },
+          {
+            name: 'element-plus',
+            var: 'ElementPlus',
+            path: 'https://unpkg.com/element-plus@2.2.26/dist/index.full.js'
+          },
+          {
+            name: 'axios',
+            var: 'axios',
+            path: 'https://unpkg.com/axios@1.2.1/dist/axios.min.js'
+          },
+          {
+            name: 'vue-demi',
+            var: 'VueDemi',
+            path: 'https://unpkg.com/vue-demi@0.13.11/lib/index.iife.js'
+          },
+          {
+              name: 'pinia',
+              var: 'Pinia',
+              path: 'https://unpkg.com/pinia@2.0.28/dist/pinia.iife.js'
+          },
+        ]
+      })
+    ],
     resolve: {
       // 配置路径别名
       alias: {
         '@': '/src'
       },
-      extensions: ['.js', '.vue']
+      extensions: ['.js', '.vue'],
     },
     server: {
       // host: '0.0.0.0',
